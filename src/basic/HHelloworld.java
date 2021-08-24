798fc1ff04cd3161fea23682ca7d91bc36ac9672
louis-gin <58906764+louis-gin@users.noreply.github.com>
2021-08-24 20:20:14 +0630

    Update HHelloworld.java

--- src/basic/HHelloworld.java
+++ src/basic/HHelloworld.java
@@ -5,6 +5,7 @@ public class HHelloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("This is added code");
+		System.out.print("This is added code from GitHub");
 
 	}
 
